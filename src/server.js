71a48968c97df5a74c4416a0abaeda74016ce652
James Heggs <james.heggs@techreturners.com>
2021-11-16 23:01:04 +0000

    Re-locate environment check

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
+const environment = process.env.NODE_ENV || 'dev';
 const app = require('./app');
 const populateDummyData = require('./database/database_seed');
-
-const environment = process.env.NODE_ENV || 'dev';
 
 app.listen(3000, () => {
   console.log('Express started on port 3000');
